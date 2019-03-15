e1029a443596c958d3ef71ef4b3bef851ae0ed22
MagicFame <jeremie.uzan@gmail.com>
2019-03-15 12:13:49 +0100

    Fetching data using API

---
+++ app/src/main/java/com/tilted/magicfame/cookeat/FetchData.java
@@ -0,0 +1,4 @@
+package com.tilted.magicfame.cookeat;
+
+public class FetchData {
+}
--- app/src/main/java/com/tilted/magicfame/cookeat/MainActivity.java
+++ app/src/main/java/com/tilted/magicfame/cookeat/MainActivity.java
@@ -2,12 +2,62 @@ package com.tilted.magicfame.cookeat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String API_KEY = "93e5b880b602c4df65119c7236a61fe7";
+    private final static String API_ID = "71b510d1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void onClick(View v){
+        String research = ((EditText)(findViewById(R.id.editText2))).getText().toString();
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        try {
+            String APIURL = "https://api.edamam.com/search?app_id=" + API_ID + "&app_key="
+                    + API_KEY + "&from=0&to=9&q=" + research;
+
+            URL url = new URL(APIURL);
+            System.out.println(url);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.getInputStream();
+
+
+        } catch (IOException e) {
+            Log.e("PlaceholderFragment", "Error ", e);
+        }
+        finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
+    }
+
+
 }
