6436cc6d09e49e84d2f9c5a617b9ca7873352fd3
MagicFame <jeremie.uzan@gmail.com>
2019-03-15 08:23:55 +0100

    Initialisation commit

---
+++ app/src/main/java/com/tilted/magicfame/cookeat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tilted.magicfame.cookeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
